cb39d4c7b7439d611f6ca87d494570463773d75a
Jirka Neumann <jirka.neumann@gmx.de>
2025-06-28 02:14:17 +0200

    beta read files and plots

--- R-bulkFilesPlot.R
+++ R-bulkFilesPlot.R
@@ -1,47 +1,48 @@
-library(officer)
+#library(officer)
 library(rstudioapi)
-#library(readxl)
+library(readxl)
+library(writexl)
 
 
 this_file = rstudioapi::getActiveDocumentContext()$path
 path = box::file()
 check_path = unlist(strsplit(this_file, split = "/"))
 check_path = paste0(check_path[1:length(check_path)-1], collapse="/")
-if (check_path != path){warning("There might be issues related to the path of files.", call. = TRUE, immediate. = FALSE, domain = NULL)}
 
-
-setwd(file.path(path, "data"))
-allfiles = dir()
-print(allfiles)
-
-
-data_df = read.csv2("results-survey983757_Test1.csv")
-#data_df = read_xlsx("results-survey231835.xlsx")
-
-#--------------
-'
-data_files = list.files(pattern="*.csv", full.names=F) #--> Namensliste erstellen
-data_list = lapply(data_files, read.csv2, header=T, na.strings=c("", " ", "NA"), encoding="UTF-8")
-data_df = data.frame()
-for(i in 1:length(data_files)){
-  temp_df = data_list[[i]]
-  data_df = rbind(data_df, temp_df)
+if (check_path != path){
+  warning("There might be issues related to the path of files.", call. = TRUE, immediate. = FALSE, domain = NULL)
+  }else{
+  setwd(file.path(path, "data"))
+  allfiles = dir()
+  #print(allfiles)
 }
-rm(i, data_files, temp_df)
-write.csv2(data_df, "results.csv")
-'
-#--------------
+
+source("insert_testfiles.R")
+
+data_files = list.files(pattern="*.xlsx", full.names=F)
+#data_list = lapply(data_files, read_xlsx)
+
+all_data = read_xlsx("_template.xlsx", col_names = T)
+print(names(all_data))
+
+for(i in 3:length(data_files)){
+  print(i)
+  next_file = read_xlsx(data_files[i], col_names = T)
+  colnames(next_file) = names(all_data)
+  all_data = rbind(all_data, next_file)
+}
+
+#write_xlsx(all_data, "result_file.xlsx")
 
 
 
-#--> plots 
-#------------------------------#
+#---- plots -----------#
+plot_this = all_data[17]
+graftitle = names(plot_this)
 
 
 
-graftitle = names(data_df[10])
-
-ergebnis = table(data_df[10])
+ergebnis = table(plot_this)
 #-------- plot -------
 yAchse = c(0, max(ergebnis) + round(max(ergebnis*0.2), 0))
 if(max(ergebnis) == 1 ){yAchse = c(0, 2)} #y-Achse ggf. auf 2 setzen
@@ -51,25 +52,29 @@ text(ergebnis, labels = c(as.character(ergebnis)), cex = 1, col = "blue", pos = 
 #---------------------
 
 
-ergebnis_2 = as.data.frame(table(data_df[10]))
+ergebnis_2 = as.data.frame(table(plot_this))
 #-------- plot -------
-#require(ggplot2)
+require(ggplot2)
 
-p1 = ggplot(ergebnis_2, aes(x=Var1, y=Freq)) + 
+p1 = ggplot(ergebnis_2, aes(x=ergebnis_2[, 1], y=Freq)) + 
   geom_bar(stat = "identity", color = "blue", fill="grey",) + 
   geom_text(aes(label=Freq), vjust=-0.3, size=3.5) +
-  labs(title = graftitle, y = "Häufigkeiten", x = "x-Werte") + 
+  labs(title = paste0(graftitle, "\n")) + 
+  ylim(0, max(ergebnis_2$Freq)+round(max(ergebnis_2$Freq*0.25), 0)+1)+
   theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
-    axis.line.y = element_line(colour = "black"),
+    #axis.line.y = element_line(colour = "black"),
+    axis.line.y = element_blank(),
     axis.line.x = element_blank(),
     axis.title.x=element_blank(), #remove axis title
     axis.title.y=element_blank(), #remove axis title
-    #axis.text.x=element_blank(),  #remove axis labels
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-    axis.ticks.x=element_blank()  #remove axis ticks
+    axis.text.y=element_blank(),  #remove axis labels
+    #axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    axis.text.x = element_text(angle = 35, vjust = 0.5, hjust=.35),
+    axis.ticks.x=element_blank(),  #remove axis ticks
+    axis.ticks.y=element_blank()  #remove axis ticks
   )
 
 plot(p1)
